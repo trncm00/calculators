32deca60d49f07bcaba2d2559896ae4c5e2a73bc
Terence Minerbrook <terence.minerbrook@gmail.com>
2023-01-17 10:28:17 -0500

    resolve to eschew getElementsByTageName

--- scripts/calculator_js.js
+++ scripts/calculator_js.js
@@ -1,17 +1,26 @@
-// 
+// I would rather not have to add classes to html,
+//i would rather just use js to grab the buttons tags
+//and act on them, it is just too tricky.
 
 //maybe i can use this for an array later, this 
 //not working with addElementById
-// let buttons=document.getElementsByTagName("button");
-
+const buttons=document.getElementsByTagName("button");
+const eventTst=(buttonPar)=>{
+  console.log(`The mouse click event on the ${buttonPar}`)
+}
+// const buttonEvent = buttons[0].addEventListener('click',eventTst)
+console.log(`this is document.getElementsByTagName("button") ${buttons} `)
 let btnArr = Array();
-for(let button in buttons){
-btnArr.push(button)
+for(let i = 0; i>=buttons.length;i++){
+// btnArr.push(buttons[i])b
+let button = buttons[i];
+button.addEventListener('click',eventTst)
+console.log(`This, the value (${button}) is a ${typeof button }.`)
+
 };
 
-console.log(typeof(buttons))
-console.log(btnArr)
-
+// console.log(typeof(buttons))
+// console.log(`this is an array of buttons, ${btnArr}`)
 /*
 
 //This statement returns an array
