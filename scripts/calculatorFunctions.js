5aaaa599e82bffe626fe459216897a6bc475f497
Terence Minerbrook <terence.minerbrook@gmail.com>
2023-02-04 11:07:36 -0500

    operations on numbers larger than ten

--- scripts/calculatorFunctions.js
+++ scripts/calculatorFunctions.js
@@ -1,48 +1,7 @@
-// let displayEl = document.getElementsByClassName("ClcDisplay");
 let joinedString = new String();
 let numberfiedString = new Number();
-let reference_number = new Number();
 let mathematicalSymbol = new String();
 let expressionValue = new Number();
-
-let displayList = new Array();
-let numberList = new Array();
-let expressionList = new Array();
-
-let buttons = document.querySelectorAll("button");
-console.log(buttons);
-let CalculatorEffects = buttons.forEach((button) =>
-  button.addEventListener("click", () => {
-    let buttonPressed = button.textContent;
-    console.log(`You clicked: ${buttonPressed}`);
-    setDisplayList(buttonPressed);
-    document.body.firstElementChild.innerText = joinString(displayList);
-    //--------------------------------do-not-code-above-here-----------------------------//
-    if (!isNaN(buttonPressed)) {
-      setNumberList(buttonPressed);
-      // setExpressionList(numberfiedString); //too early
-      // numberList = [];
-    } else if (isNaN(buttonPressed) || buttonPressed != "=") {
-      setExpressionList(numberfiedString); //too early
-      numberList = [];
-      if (isNaN(buttonPressed) && buttonPressed != "=") {
-        setSymbol(buttonPressed);
-      } else {
-        displayListEraser();
-        calculatorControlFunction(expressionList);
-      }
-    }
-    // {
-    //   setExpressionList(numberfiedString); //too early
-    //   numberList = [];
-    // } else if (isNaN(buttonPressed) && buttonPressed != "=") {
-    //   setSymbol(buttonPressed);
-    // } else {
-    //   displayListEraser();
-    //   calculatorControlFunction(expressionList);
-    // }
-  })
-);
 
 //-------------------------------basic--functionality---------------------------------//
 function setDisplayList(parrPressed) {
@@ -76,7 +35,6 @@ function setNumberList(param) {
   numberList.push(param); //send to numberList
   joinedString = joinString(numberList); //join numberlist
   numberfiedString = parseString(joinedString); //numberfied string
-  reference_number = numberfiedString;
 }
 
 function calculatorControlFunction(arrPar) {
